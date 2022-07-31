0dc14d6c87118686dd248460759f7b1dbdf0018a
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-31 13:40:30 -0500

    Lecture 8

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
-  // data, functions
-  //   template: '<h2>I am the template</h2>',
+  data() {
+    return {
+      title: 'The Final Empire',
+      author: 'Brandon Sanderson',
+      age: 46,
+    };
+  },
 });
 
 app.mount('#app');
