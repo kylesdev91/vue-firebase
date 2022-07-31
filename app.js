54c658d70dd527f75e150acf5a8a9ebfa9649324
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-31 15:57:56 -0500

    Lecture 11

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const app = Vue.createApp({
       title: 'Magic Tree House',
       author: 'Mary Pope Ashborne',
       age: 73,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toogleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
