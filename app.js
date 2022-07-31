a37bfaed81f1540f178df79185ff75d920e817ab
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-31 16:48:40 -0500

    Lecture 13

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'www.thenetninja.co.uk',
       showBooks: true,
       books: [
-        { title: 'Diary of a Wimpy Kid', author: 'Jeff Kinney' },
-        { title: 'Dinosaurs Before Dark', author: 'Mary Pope Osborne' },
-        { title: 'Mummies Before Moring', author: 'Mary Pope Osborne' },
+        {
+          title: 'Diary of a Wimpy Kid',
+          author: 'Jeff Kinney',
+          img: 'assets/1.jpg',
+        },
+        {
+          title: 'Dinosaurs Before Dark',
+          author: 'Mary Pope Osborne',
+          img: 'assets/2.jpg',
+        },
+        {
+          title: 'Mummies Before Moring',
+          author: 'Mary Pope Osborne',
+          img: 'assets/3.jpg',
+        },
       ],
     };
   },
